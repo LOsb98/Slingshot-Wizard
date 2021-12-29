900e2d608d9890e8dac809e56cb648b3cf7698d2
Liam Osborn <liam.osborn@toka.uk>
2021-12-29 18:46:54 +0000

    Unpausing time on level load

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,17 +6,23 @@ public class LevelLoader : MonoBehaviour
     public void LoadLevel(int levelID)
     {
         SceneManager.LoadScene(levelID);
+
+        Time.timeScale = 1f;
     }
 
     public void NextLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene + 1);
+
+        Time.timeScale = 1f;
     }
 
     public void ReloadLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
+
+        Time.timeScale = 1f;
     }
 }
