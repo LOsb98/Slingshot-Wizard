ba9b42eade69754f10fdd7d5694686740b6d5aa4
Liam Osborn <liam.osborn@toka.uk>
2021-12-29 18:45:43 +0000

    Getting mouse position in FixedUpate() to fix flickering rope position

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
-    public Animator animator;
-    public DistanceJoint2D grapple;
-    public LineRenderer rope;
-    public Transform grapplePos;
+    public Animator _animator;
+    public DistanceJoint2D _grappleJoint;
+    public LineRenderer _ropeLineRenderer;
+    public Transform _grappleOrigin;
 
-    public LayerMask grabLayer;
-    public LayerMask nonGrabLayer;
-    public Vector3 mousePos;
+    public LayerMask _grabLayer;
+    public LayerMask _nonGrabLayer;
+    public Vector3 _mousePos;
 
-    public float grappleMax;
+    public float _grappleMaxLength;
 
-    public Color ropeColor;
-    public Color tooFarColor;
-    public Color okColor;
+    public Color _ropeColour;
+    public Color _outOfGrappleRangeColor;
+    public Color _inGrappleRangeColor;
 
-    void Update()
+    private void FixedUpdate()
     {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _mousePos.z = 0;
+    }
+    private void Update()
+    {
 
         //Two raycasts are used, one to check for grabbable surfaces and another for non-grabbable surfaces
         //Each uses a different layer mask
-        RaycastHit2D grappleCheck = Physics2D.Raycast(grapplePos.position, (mousePos - grapplePos.position), grappleMax, grabLayer, 0);
-        RaycastHit2D nonGrabCheck = Physics2D.Raycast(grapplePos.position, (mousePos - grapplePos.position), grappleMax, nonGrabLayer, 0);
+        RaycastHit2D grappleCheck = Physics2D.Raycast(_grappleOrigin.position, (_mousePos - _grappleOrigin.position), _grappleMaxLength, _grabLayer, 0);
+        RaycastHit2D nonGrabCheck = Physics2D.Raycast(_grappleOrigin.position, (_mousePos - _grappleOrigin.position), _grappleMaxLength, _nonGrabLayer, 0);
 
 
 
@@ -36,22 +39,22 @@ public class Grapple : MonoBehaviour
         {
             //If a grabbable surface is in range but a non-grabbable surface is blocking it, the grapple won't attach
             //This is done by using both raycasts and checking which hits a surface first
-            if (grappleCheck && Vector2.Distance(nonGrabCheck.point, grapplePos.position) > Vector2.Distance(grappleCheck.point, grapplePos.position))
+            if (grappleCheck && Vector2.Distance(nonGrabCheck.point, _grappleOrigin.position) > Vector2.Distance(grappleCheck.point, _grappleOrigin.position))
             {
                 //If a grabbable surface is in range when the mouse is clicked, attach the grapple to it and set its max length
-                grapple.connectedAnchor = grappleCheck.point;
-                grapple.distance = Vector2.Distance(grapple.connectedAnchor, grapplePos.position);
-                grapple.enabled = true;
+                _grappleJoint.connectedAnchor = grappleCheck.point;
+                _grappleJoint.distance = Vector2.Distance(_grappleJoint.connectedAnchor, _grappleOrigin.position);
+                _grappleJoint.enabled = true;
             }
         }
 
         else if (Input.GetMouseButtonUp(0))
         {
             //Disable the grapple when the mouse button is released
-            grapple.enabled = false;
+            _grappleJoint.enabled = false;
         }
-        animator.SetFloat("GrappleHeight", grapple.connectedAnchor.y - grapplePos.position.y);
-        animator.SetBool("Grappled", grapple.enabled);
+        _animator.SetFloat("GrappleHeight", _grappleJoint.connectedAnchor.y - _grappleOrigin.position.y);
+        _animator.SetBool("Grappled", _grappleJoint.enabled);
 
         //grappleCheck and nonGrabCheck are passed into a separate method for readability purposes
         DrawRope(grappleCheck, nonGrabCheck);
@@ -59,36 +62,36 @@ public class Grapple : MonoBehaviour
 
     private void DrawRope(RaycastHit2D grappleCheck, RaycastHit2D nonGrabCheck)
     {
-        rope.SetPosition(0, grapplePos.position);
-        if (grapple.enabled)
+        _ropeLineRenderer.SetPosition(0, _grappleOrigin.position);
+        if (_grappleJoint.enabled)
         {
-            rope.startColor = ropeColor;
-            rope.endColor = ropeColor;
-            rope.SetPosition(1, grapple.connectedAnchor);
-            //If the grapple is currently active the rope can be drawn and the method can be escaped, nothing else needs to be calculated
+            _ropeLineRenderer.startColor = _ropeColour;
+            _ropeLineRenderer.endColor = _ropeColour;
+            _ropeLineRenderer.SetPosition(1, _grappleJoint.connectedAnchor);
+            //If the grapple is currently active the _ropeLineRenderer can be drawn and the method can be escaped, nothing else needs to be calculated
             return;
         }
 
-        else if (grappleCheck && Vector2.Distance(nonGrabCheck.point, grapplePos.position) > Vector2.Distance(grappleCheck.point, grapplePos.position))
+        else if (grappleCheck && Vector2.Distance(nonGrabCheck.point, _grappleOrigin.position) > Vector2.Distance(grappleCheck.point, _grappleOrigin.position))
         {
             //If a grabbable surface is detected within range the end of the line will snap to it, prevents it from sticking out behind surfaces
-            rope.SetPosition(1, grappleCheck.point);
-            rope.startColor = okColor;
-            rope.endColor = okColor;
+            _ropeLineRenderer.SetPosition(1, grappleCheck.point);
+            _ropeLineRenderer.startColor = _inGrappleRangeColor;
+            _ropeLineRenderer.endColor = _inGrappleRangeColor;
         }
         else
         {
-            //If nothing grabbable is in range the rope will point towards the mouse and maintain a fixed length
+            //If nothing grabbable is in range the _ropeLineRenderer will point towards the mouse and maintain a fixed length
             //This gives better feedback than the old solution where the red line would follow the mouse no matter what
-            rope.SetPosition(1, ((mousePos - grapplePos.position).normalized * grappleMax) + grapplePos.position);
-            rope.startColor = tooFarColor;
-            rope.endColor = tooFarColor;
+            _ropeLineRenderer.SetPosition(1, ((_mousePos - _grappleOrigin.position).normalized * _grappleMaxLength) + _grappleOrigin.position);
+            _ropeLineRenderer.startColor = _outOfGrappleRangeColor;
+            _ropeLineRenderer.endColor = _outOfGrappleRangeColor;
         }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(grapplePos.position, mousePos);
+        Gizmos.DrawLine(_grappleOrigin.position, _mousePos);
     }
 }
